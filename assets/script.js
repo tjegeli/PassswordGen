6f325a65ca358ebe991cc738a60c82501bd7e558
Leo <tjegeli@gmail.com>
2021-11-08 00:38:11 +0000

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var symbols =  ["!", "@", "£", "$", "%", "&", "*", "_", "?" ];
+ // var lowerCase = ["a, "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o" ];
+  var uppperCase = ["A", "B", "C", "D", "E", "F", "G", "H"]
+  passwordText.value = password;
+
+}
+// create a password between 8 - 128 characters 
+for ( var i = 0; i < password; i++) { 
+
+window.prompt + ("Confirm number of ")
+
+
+
+// create a pop up window from a button being clicked
+generateBtn.addEventListener("click", function() { 
+  //if (generateBtn.click === true) {
+  //mode = "light";
+  window.prompt("Hello"); 
+}
+ 
+  
+
+  // If mode is light, apply dark background 
+ 
+
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
+
+// Promtps - Length 8-12
+             // lower case or uppercase
+             // numeric characters and
+             //  special characters
+
+             // when answered these prompts the input should be validated.
+             //When all prompts are answered a password should be generated
+
+    
+
+
+
+
+
+        
